266a52be2a05e1820c77f3f8136fcbfdfc18b370
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-02-01 22:06:26 -0300

    Estructura HTML y estilo css

---
+++ assets/js/desafio.js
@@ -0,0 +1,23 @@
+// Agregar elementos dinamicamente en una página.
+// Agreagr tareas y al apretar el boton se actaulice la lista en la página
+
+/* requerimientos */
+
+// 1 - Agregar tareas con descripción al llenar el input y presionar el botón agregar tarea, la es agregada al arreglo y luego la lista 
+ // en la pagina web se actualiza 
+
+// 2 - Borrar una taera al hacer click en el botón que acompaña a la tarea. Se debe borrar el dato del arreglo y actualizar la lista
+
+// 3 - Contar el total de tareas, mantener actualiza la cuenta cuando se agrege o se borre una tarea.
+
+// 4 - Marcar una tarea como completada al hacer click en un checkbox.
+
+/* pistas */
+
+// - Agregar al objeto el estado completado:
+// - Iniciar las tareas con completado : False
+// - al hacer click en el botón cambiar, se debe buscar el elemento por el indicie y luego cambiar el estado a completado true.
+
+// 5 - Contar el total de tareas realizadas.
+
+// 6 - El código incluye al menos 3 tareas iniciales en el arrego y estas se muestran en la página web recorriendo el arreglo (for of || forEach)
